29cb7815b825c00f7c040ecbf8b791d265747241
Phern17 <mphern17@gmail.com>
2022-01-25 16:52:15 +0800

    allow fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -43,7 +43,7 @@ export const getStaticPaths = async () => {
   });
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: places.map((place) => ({
       params: { meetupId: place._id.toString() },
     })),
